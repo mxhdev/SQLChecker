6ce8ec19ff425805e5585891aeb97dedab2d84c5
Timo <timoei@users.noreply.github.com>
2015-12-08 10:00:53 +0100

    Data generator improvements.

--- src/sqlchecker/test/DataGenerator.java
+++ src/sqlchecker/test/DataGenerator.java
@@ -6,14 +6,16 @@ public class DataGenerator {
 		// TODO Auto-generated method stub
 		
 		//Anzahl der Datensätze in Tabelle i
-		int i = 15;
+		int i = 50;
 		
 		//Anzahl der Datensätze in Tabelle j
-		int j = 24;
+		int j = 12;
 		int zufallszahl1;
 		int zufallszahl2;
+		int zufallszahl3;
+		int zufallszahl4;
 		
-		//PNR der Ärzte
+		//PNR der Ärzte Anzahl = 13
 		int[] arzt = new int[]{4,5,8,12,13,14,23,33,34,35,40,43,45};
 		
 		//Medikament Anzahl = 46
@@ -23,9 +25,11 @@ public class DataGenerator {
 		String[] krankheit = new String[]{"Alternaria","Apoplex","Arderum","Commotio cerebri","Effectus","Everin","Fraktur","FussiJuckus","Gastroenteritis","Gripus","Hatschi","HIV","Husterus","Incidento","Infarkt","Insomnia","KopfAua","Lebensmittelintoxikation","Malitosus","Namrium","NervusKlemus","Ohrus","Propus","Sarati","Solares","Wundus"};
 		
 		for(int a = 1; a <= i; a++){
-			zufallszahl1 = (int)(Math.random() * i) + 0; 
-			zufallszahl2 = (int)(Math.random() * j) + 0; 
-			System.out.println("('"+krankheit[a]+"',"+zufallszahl1+"),");
+			zufallszahl1 = (int)(Math.random() * i) + 1; 
+			zufallszahl2 = (int)(Math.random() * j) + 0;
+			zufallszahl3 = (int)(Math.random() * 46) + 0;
+			zufallszahl4 = (int)(Math.random() * 8000000) + 1;
+			System.out.println("("+a+","+arzt[zufallszahl2]+", '"+ medi[zufallszahl3] +"', now(), "+zufallszahl4 +", "+zufallszahl1+"),");
 			
 		}
 		
