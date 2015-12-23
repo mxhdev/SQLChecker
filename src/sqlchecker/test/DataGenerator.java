d12fe0aa456c094c43971c85bd5d62c3e8041334
Timo <timoei@users.noreply.github.com>
2015-12-23 13:44:33 +0100

    Changed for providing test data.

--- src/sqlchecker/test/DataGenerator.java
+++ src/sqlchecker/test/DataGenerator.java
@@ -1,12 +1,14 @@
 package sqlchecker.test;
 
+import java.sql.Timestamp;
+
 public class DataGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Anzahl der Datensätze in Tabelle i
-		int i = 50;
+		int i = 45;
 		
 		//Anzahl der Datensätze in Tabelle j
 		int j = 12;
@@ -14,6 +16,8 @@ public class DataGenerator {
 		int zufallszahl2;
 		int zufallszahl3;
 		int zufallszahl4;
+		int zufallszahl5;
+		int zufallszahl6;
 		
 		//PNR der Ärzte Anzahl = 13
 		int[] arzt = new int[]{4,5,8,12,13,14,23,33,34,35,40,43,45};
@@ -23,16 +27,26 @@ public class DataGenerator {
 		
 		//Krankheit Anzahl = 25
 		String[] krankheit = new String[]{"Alternaria","Apoplex","Arderum","Commotio cerebri","Effectus","Everin","Fraktur","FussiJuckus","Gastroenteritis","Gripus","Hatschi","HIV","Husterus","Incidento","Infarkt","Insomnia","KopfAua","Lebensmittelintoxikation","Malitosus","Namrium","NervusKlemus","Ohrus","Propus","Sarati","Solares","Wundus"};
-		
 		for(int a = 1; a <= i; a++){
 			zufallszahl1 = (int)(Math.random() * i) + 1; 
 			zufallszahl2 = (int)(Math.random() * j) + 0;
-			zufallszahl3 = (int)(Math.random() * 46) + 0;
-			zufallszahl4 = (int)(Math.random() * 8000000) + 1;
-			System.out.println("("+a+","+arzt[zufallszahl2]+", '"+ medi[zufallszahl3] +"', now(), "+zufallszahl4 +", "+zufallszahl1+"),");
+			zufallszahl3 = (int)(Math.random() * 25) + 0;
+			zufallszahl4 = (int)(Math.random() * 832) + 1;
+			zufallszahl5 = (int)(Math.random() * 10) + 1;
+			zufallszahl6 = (int)(Math.random() * 59) + 0;
+			System.out.println("('"+medi[a]+"', '"+ krankheit[zufallszahl3] +"'),");
 			
 		}
 		
+	}
+	
+	public static String timeStampGenerator(){
+		long offset = Timestamp.valueOf("2016-01-01 00:00:00").getTime();
+		long end = Timestamp.valueOf("2016-02-01 00:00:00").getTime();
+		long diff = end - offset + 1;
+		Timestamp rand = new Timestamp(offset + (long)(Math.random() * diff));
+		
+		return rand.toString();
 	}
 
 }
