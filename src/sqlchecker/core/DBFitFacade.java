4c1d2f91c4e53178dc62eff2b10f08775dac76ba
Timo <timoei@users.noreply.github.com>
2016-01-03 15:09:36 +0100

    Added output of the HTML-SQL string to test it locally in DBFit.

--- src/sqlchecker/core/DBFitFacade.java
+++ src/sqlchecker/core/DBFitFacade.java
@@ -67,6 +67,7 @@ public class DBFitFacade {
 		try {
 			// init connection
 			tester = init();
+			System.out.println(sqlhtml);
 			
 			// parse & execute the submission 
 			Parse target = new Parse(sqlhtml);
