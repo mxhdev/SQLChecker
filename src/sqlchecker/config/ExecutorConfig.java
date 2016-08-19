f8d515a969bec16d1d803b08601f58899e5d1d5f
Max <mhofmann91@gmail.com>
2016-08-19 10:37:53 +0200

    Added javadoc to container classes

--- src/sqlchecker/config/ExecutorConfig.java
+++ src/sqlchecker/config/ExecutorConfig.java
@@ -2,19 +2,48 @@ package sqlchecker.config;
 
 public class ExecutorConfig extends Config {
 
-	
+	/**
+	 * The folder in which the submissions can be found
+	 */
 	private String submissionPath = "";
 	
+	/**
+	 * The path leading to the solution file which should be 
+	 * used for checking submissions
+	 */
 	private String solutionPath = "";
 	
+	/**
+	 * This is the place where the logs and results files will be created
+	 */
 	private String assignmentPath = "";
 	
+	
+	/**
+	 * Should static queries be allowed in student submissions?
+	 */
 	private boolean staticEnabled = false;
 	
 	
+	/**
+	 * Creates a configuration container
+	 * @param submPath The folder in which the submissions can be found
+	 * @param solPath The path leading to the solution file which should be 
+	 * used for checking submissions
+	 * @param agnPath This is the place where the logs and results files will be created
+	 * @param staticEnabled Should static queries be allowed in student submissions?
+	 * @param user database user
+	 * @param pw database user password
+	 * @param host database host name (e.g. localhost)
+	 * @param db database name (empty = default)
+	 * @param resetPath Path leading to the reset file. This file is important for
+	 * making sure that the database is always in the same state before checking
+	 * a submission
+	 * 
+	 */
 	public ExecutorConfig(String submPath, String solPath, String agnPath
-			, String user, String pw, String host, String db, String resetPath
-			, boolean staticEnabled) {
+			, boolean staticEnabled , String user, String pw, String host
+			, String db, String resetPath) {
 		
 		super(user, pw, host, db, resetPath);
 		
--- src/sqlchecker/config/GeneratorConfig.java
+++ src/sqlchecker/config/GeneratorConfig.java
@@ -2,13 +2,40 @@ package sqlchecker.config;
 
 public class GeneratorConfig extends Config {
 
+	
+	/**
+	 * Path of the "raw" file, from which the final solution file 
+	 * should be created
+	 */
 	private String inPath = "";
 	
+	/**
+	 * The path of the output solution file (e.g. data/tmp/solution.txt)
+	 */
 	private String outPath = "";
 	
+	/**
+	 * The path of the output sample submission for the created 
+	 * solution file (e.g. data/tmp/sample.sql)
+	 */
 	private String samplePath = "";
 	
 	
+	/**
+	 * 
+	 * @param inputPath Path of the "raw" file, from which the final solution file 
+	 * should be created
+	 * @param outputPath The path of the output solution file (e.g. data/tmp/solution.txt)
+	 * @param samplePath The path of the output sample submission for the created 
+	 * solution file (e.g. data/tmp/sample.sql)
+	 * @param user database user 
+	 * @param pw database user password
+	 * @param host database host name (e.g. localhost)
+	 * @param db database name (empty = default)
+	 * @param resetPath Path leading to the reset file. This file is important for
+	 * making sure that the database is always in the same state before checking
+	 * a submission
+	 */
 	public GeneratorConfig(String inputPath
 			, String outputPath, String samplePath, String user
 			, String pw, String host, String db, String resetPath) {
@@ -25,15 +52,32 @@ public class GeneratorConfig extends Config {
 	 * Getters
 	 */
 	
+	
+	
+	/**
+	 * 
+	 * @return Path of the "raw" file, from which the final solution file 
+	 * should be created
+	 */
 	public String getInputPath() {
 		return this.inPath;
 	}
 	
 	
+	/**
+	 * 
+	 * @return The path of the output solution file (e.g. data/tmp/solution.txt)
+	 */
 	public String getOutputPath() {
 		return this.outPath;
 	}
 	
+	
+	/**
+	 * 
+	 * @return The path of the output sample submission for the created 
+	 * solution file (e.g. data/tmp/sample.sql)
+	 */
 	public String getSamplePath() {
 		return this.samplePath;
 	}
